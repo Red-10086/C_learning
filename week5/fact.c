203f806592bdb43e4d3f7b2cd5408dadedee35fc
Red-10086 <2495457358@qq.com>
2025-10-31 16:09:08 +0800

    week5

---
+++ week5/fact.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int N = 0;
+    int fact = 1;
+    
+    //输入
+    printf("请输入一个正整数：");
+    scanf("%d", &N);
+
+    //阶乘
+    for(int i=1; i<=N; i++)
+    {
+        fact*=i;
+        printf("i=%d,fact=%d\n", i, fact);
+    }
+    printf("%d的阶乘等于 %d", N, fact);
+
+    return 0;
+}
