3d923f19aa7d39d718330a10e96869c04eec9526
Red-10086 <2495457358@qq.com>
2026-02-21 20:15:17 +0800

    2026.2.21.2

--- week7/exercise7_4.c
+++ week7/exercise7_4.c
@@ -1,49 +1,96 @@
 /* 
-编写set函数，返回将无符号整数x的第pos位设为1后的值
+练习7-4:
+    编写set函数，返回将无符号整数x的第pos位设为1后的值
     unsigned set(unsigned x, int pos){codes}
     编写reset函数，返回将无符号整数x的第pos位设为0后的值
     unsigned reset(unsigned x, int pos){codes}
     编写inverse函数，返回将无符号整数x的第pos位取反后的值
-    unsigned invers(unsigned x, int pos){codes}
+    unsigned inverse(unsigned x, int pos){codes}
 */
 
 #include <stdio.h>
 
-#define byte_to_bit 8
-
 unsigned set(unsigned x, int pos);/* pos位写1 */
 unsigned reset(unsigned x, int pos);/* pos位写0 */
-unsigned invers(unsigned x, int pos);/* pos位取反 */
+unsigned inverse(unsigned x, int pos);/* pos位取反 */
+unsigned mask_OR(int pos);/* 返回掩码，函数外将掩码按位或 | ,可以给特定位写1 */
+unsigned mask_AND(int pos);/* 返回掩码，函数外将掩码按位与 & ,可以给特定位写0 */
+unsigned mask_XOR(int pos);/* 返回掩码，函数外将掩码按位异或 ^ ,可以反转特定位 */
 void print_bits(unsigned x);/* 打印二进制数 */
 
 int main(void)
 {
     unsigned number = 0;/* 测试数 */
     unsigned count = 0;/* 要修改的位数 */
-
-    printf("请输入一个非负整数:");
+    
+    /* 提示输入 */
+    printf("请输入一个非负整数:\n");
     scanf("%u", &number);
-    printf("请输入要修改的位数:");
+    printf("例如:数字 5 的二进制是 101,最低位(第1位)是1,第2位是0,第3位是1.\n");
+    printf("请输入您想修改的位数(1~32):\n");
     scanf("%u", &count);
-
-    print_bits(set(number, count)); putchar('\n');
-    print_bits(reset(number, count)); putchar('\n');
-    print_bits(inverse(number, count)); putchar('\n');
-
+    
+    /* 输出对应值 */
+    printf("以下依次是:%u、第%u位写1、第%u位写0、反转第%u位 的二进制形式\n", number, count, count, count);
+    print_bits(number);                     putchar('\n');
+    print_bits(set(number, count));         putchar('\n');
+    print_bits(reset(number, count));       putchar('\n');
+    print_bits(inverse(number, count));     putchar('\n');
+    
     return 0;
 }
 
-void print_bits(unsigned x)
+unsigned set(unsigned x, int pos)/* pos位写1 */
+{
+    return x | mask_OR(pos);/* 用或写1 */
+}
+
+unsigned reset(unsigned x, int pos)/* pos位写0 */
+{
+    return x & mask_AND(pos);/* 用与写0 */
+}
+unsigned inverse(unsigned x, int pos)/* pos位取反 */
+{
+    return x ^ mask_XOR(pos);
+}
+
+unsigned mask_OR(int pos)/* 返回掩码，函数外将掩码按位或 | ,可以给特定位写1 */
+{
+    unsigned a = 1U;/* 比如要获得第3(pos)位为1的掩码0100,应将0001左移3-1位*/
+    a = a << (pos-1);
+    
+    return a;
+}
+
+unsigned mask_AND(int pos)/* 返回掩码，函数外将掩码按位与 & ,可以给特定位写0 */
+{
+    unsigned a = 1U;
+    a = a << (pos-1);/* 比如要获得第3(pos)位为1的掩码0100,应将0001左移3-1位*/
+    a = ~a;/* 获得一个除自低向高第 pos 位外,其余全1的掩码 */
+    
+    return a;
+}
+
+unsigned mask_XOR(int pos)/* 返回掩码，函数外将掩码按位异或 ^ ,可以反转特定位 */
+{
+    unsigned a = 1U;
+    a = a << (pos-1);/* 比如要获得第3(pos)位为1的掩码0100,应将0001左移3-1位*/
+    
+    return a;
+}
+
+
+void print_bits(unsigned x)/* 输入无符号整数x,函数打印其二进制形式 */
 {
     int i;
-
+    
     // 自高位起,从左向右逐位输出二进制数.
     // sizeof(unsigned)可以替换为int_bits() 
-	for (i = byte_to_bit * sizeof(unsigned) - 1; i >= 0; i--)
+    for (i = 8 * sizeof(unsigned) - 1; i >= 0; i--)/*8:byte to bit*/
     {
         //后缀U表示无符号整数,0U表示无符号整数0,1U表示无符号整数1.
         putchar(((x >> i) & 1U) ? '1' : '0');
-
+        
         //每8位空一格，最后一位不空
         if((i % 8 == 0) && (i != 0))
         {
---
+++ week7/exercise7_5.c
@@ -0,0 +1,131 @@
+/*
+练习7-count
+编写set_n函数,返回将无符号整数x的第pos位到第pos+n-1位的pos位起n位设为1后的值。
+    unsigned set_n(unsigned x, int pos, int n){codes}
+
+编写reset_n函数,返回将无符号整数x的第pos位开始的pos位起n位设为0后的值.
+    unsigned reset_n(unsigned x, int pos, int n){codes}
+
+编写inverse_n函数,返回将无符号整数x的第pos位开始的pos位起n位取反后的值.
+    unsigned inverse_n(unsigned x, int pos, int a){codes}
+*/
+
+/* 
+pos位起n位设1 等价于(或者说是后一个问题的子问题) 获取一个目标掩码,然后用按位或运算写1
+例子：低位起1-width位设1
+掩码:…… 0000 0000 1111U
+思路1(通解1):无符号整数,比特位有权重,给了位数信息即可用ΣB_i所得整数作为掩码
+思路2(通解2):已经掌握了生成单个位取1掩码的方法:unsigned mask = 1U << pos-1获得pos位为1的掩码
+按照要求得到目标掩码,将目标掩码拆解成一些 单个位取1的掩码,生成这些小掩码,然后用按位或计算出目标掩码
+思路3(高效特解):如果要获得低位起连续3位为1的掩码,可以利用减法的特性:0b1000-0b0001 = 0b0111,
+将b0111左移pos-1位,即可得从pos位起,3位都设为1的掩码
+*/
+#include <stdio.h>
+
+void print_bits(unsigned x);/* 打印二进制数 */
+unsigned mask_OR(int pos, int n);/* 返回一个掩码,函数外用或运算写1 */
+unsigned mask_AND(int pos, int n);/* 返回一个掩码,函数外用与运算写0 */
+unsigned mask_XOR(int pos, int n);/* 返回一个掩码,函数外用异或运算反转 */
+unsigned set_n(unsigned x, int pos, int n);/* 用掩码给特定位写1 */
+unsigned reset_n(unsigned x, int pos, int n);/* 用掩码给特定位写0 */
+unsigned inverse_n(unsigned x, int pos, int n);/* 用掩码反转特定位 */
+
+int main(void)
+{
+    /* 初始化 */
+    unsigned number = 0;/* 测试数 */
+    unsigned count = 0;/* 起始位 */
+    unsigned width = 0;/* 修改位宽 */
+    
+    /* 打印提示信息和读输入 */
+    printf("请输入一个非负整数:\n");
+    scanf("%u", &number);
+    printf("例如:数字 count 的二进制是 101,最低位(第1位)是1,第2位是0,第3位是1.\n");
+    printf("请输入您想修改的起始位数(1~32):\n");
+    scanf("%u", &count);
+    printf("请输入您想修改的位宽:\n");
+    scanf("%u", &width);
+    
+    /* 打印输出 */
+    printf("无符号整数%u:\n",number);
+    print_bits(number);
+    printf("无符号整数%u的第%u位起共%u位设为1后的值:\n", number, count, width);
+    print_bits(set_n(number, count, width));
+    printf("无符号整数%u的第%u位起共%u位设为0后的值:\n", number, count, width);
+    print_bits(reset_n(number, count, width));
+    printf("无符号整数%u的第%u位起共%u位取反后的值:\n", number, count, width);
+    print_bits(inverse_n(number, count, width));
+    return 0;
+}
+
+
+unsigned mask_OR(int pos, int n)/* 返回一个掩码,函数外用或运算写1 */
+{
+    unsigned a = 1U;
+    a = a << n;
+    a = a - 1U;/* 获得一个"低n位掩码" */
+    a = a << pos-1;/* 将掩码移到有效位置 */
+    
+    return a;
+}
+
+unsigned set_n(unsigned x, int pos, int n)/* 用掩码给特定位写1 */
+{
+    return x | mask_OR(pos, n);
+}
+
+
+unsigned mask_AND(int pos,int n)/* 返回一个掩码,函数外用与运算写0 */
+{
+    unsigned a = 1U;/* 0000 0001 */
+    a = a << n;/* 0010 0000 */
+    a = a - 1U;/* 获得一个"低n位全1掩码" 0001 1111*/
+
+    // a = ~a;/* 1110 0000 问题:左移unsigned int会补0,解决:先移位再反转 */
+    // a = a << pos-1;
+
+    a = a << pos-1;
+    a = ~a;
+
+    return a;
+}
+
+unsigned reset_n(unsigned x, int pos, int n)/* 用掩码给特定位写0 */
+{
+    return x & mask_AND(pos, n);
+}
+
+unsigned mask_XOR(int pos, int n)/* 返回一个掩码,函数外用异或运算反转 */
+{
+    unsigned a = 1U;
+    a = a << n;/* 例 0100 0000 */
+    a = a - 1;/* 例 0011 1111 */
+    a = a << pos-1;/* 例 0111 1110 */
+
+    return a;
+}
+
+unsigned inverse_n(unsigned x, int pos, int n)/* 用掩码反转特定位 */
+{
+    return x ^ mask_XOR(pos, n);
+}
+
+void print_bits(unsigned x)/* 输入无符号整数x,函数打印其二进制形式 */
+{
+    int i;
+    
+    // 自高位起,从左向右逐位输出二进制数.
+    // sizeof(unsigned)可以替换为int_bits() 
+    for (i = 8 * sizeof(unsigned) - 1; i >= 0; i--)/*8:byte to bit*/
+    {
+        //后缀U表示无符号整数,0U表示无符号整数0,1U表示无符号整数1.
+        putchar(((x >> i) & 1U) ? '1' : '0');
+        
+        //每8位空一格,最后一位不空
+        if((i % 8 == 0) && (i != 0))
+        {
+            putchar(' ');
+        }
+    }
+    putchar('\n');
+}
