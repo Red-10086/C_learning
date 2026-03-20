e083359891dead4d20af4ab1441894eab5dcf3d0
Red-10086 <2495457358@qq.com>
2026-03-20 21:50:38 +0800

    2026.3.20

--- week7/exercise9_8.c
+++ week7/exercise9_8.c
@@ -1,5 +1,37 @@
 /* 
-    编写如下函数,实现字符串的逆向输出.
+    练习9-8 编写如下函数,实现字符串的逆向输出.
+        void put_stringr(const char s[]){codes}
+    例如,将"SEC"显示为"CES".
+    思路1:逆序输出字符串,因为数组传进字符串之后,就没法直接用sizeof量字符串长度
+        ,可以等价于用一个数组装下s[]所有元素,然后逆序输出,但是太麻烦.
+    思路2:先数一遍-字符串长度len,然后用for(int i = len - 1; i >= 0; i--)循环打印s[i]
 */
+void put_stringr(const char s[]);
 
 #include <stdio.h>
+
+int main(void)
+{
+    put_stringr("CES");
+
+    return 0;
+}
+
+void put_stringr(const char s[])
+{
+    int len = 0;
+
+    for( ; s[len] != '\0'; len++)
+    {
+        /* 循环退出时,len表示第一个'\0'的下标,也是字符串(不含'\0'
+        部分)的长度.对于空字符串,不会进入循环，也没有步进. */
+    }
+
+    putchar('\"');/* 打印左引号 */
+    for(int i = len - 1; i >= 0; i--)/* 逆序输出,从到达'\0'之前最后一个下标逆序遍历 */
+    {
+        putchar(s[i]);
+    }
+    putchar('\"');/* 打印右引号 */
+
+}
---
+++ week7/exercise9_9.c
@@ -0,0 +1,50 @@
+/* 
+    编写如下函数,逆向显示字符串s的字符
+        void rev_string(char s[]){codes}
+    例如,若s中接受的是"SEC",则将其数组更新为"CES"
+
+    思路:数组原地倒序(不止字符串,而是几乎所有数组,字符串要对'\0'特殊处理)的方法.
+    法1:创建一个新数组,原样复制旧数组,然后逆序覆写旧数组.
+    法2:创建一个临时变量和一个装长度的变量,用sizeof量出数组长度length,然后用length/2作为for循环边界,让i遍历:0<=i<=length/2(length是偶数的话正好,是奇数的话,刚好不会越过"中线",且"中线不会被交换",即无论长度的奇偶性,都能只交换一遍-原地调头).
+    法2资源占用更少.
+
+    但是! 字符串末尾是'\0',必须特殊处理!!!
+*/
+
+#include <stdio.h>
+
+void rev_string(char s[]);/* 字符串原地调头 */
+
+int main(void)
+{
+    char str[128] = {'\0'};
+
+    printf("请输入字符串:\n");
+    scanf("%s", str);
+
+    printf("字符串正序: \"%s\" \n", str);
+
+    rev_string(str);
+    printf("字符串倒序: \"%s\" \n", str);
+
+    return 0;
+}
+
+void rev_string(char s[])/* 字符串原地调头 */
+{
+    int length = 0;/* 字符串长度 */
+    int tmp = 0;/* 交换数组元素的临时变量 */
+
+    for( ; s[length] != '\0'; length++){}/* 量字符串长度,如遇空字符串,则length还是0 */
+
+    //for(int i = 0; i <= length/2; i++) 
+    //对一般数组原地调头可行,但是对字符串呢?答:会把'\0'包进去,不可行
+
+    for(int i = 0; i <= (length-1)/2; i++)/* s[length]此时一定为'\0',剔除. */
+    {
+        tmp = s[i];
+        s[i] = s[length -1 - i];/* 2倍中点坐标减i,得i关于中点的对称点 */
+        s[length -1 -i] = tmp;
+    }
+
+}
