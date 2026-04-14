cb5381a287f2086931c914a4bbf5b988b72fa8c9
Red-10086 <2495457358@qq.com>
2026-04-14 19:59:24 +0800

    2026.4.14

---
+++ week8/exercise11_7.c
@@ -0,0 +1,72 @@
+/* 
+    练习11-7 不使用下标运算符,实现代码清单9-1的str_toupper和str_tolower函数.
+
+    // 将字符串中的英文字母转为大写字母 
+	void str_toupper(char s[])
+	{
+		int i = 0;
+		while (s[i]) {
+			s[i] = toupper(s[i]);
+			i++;
+		}
+	}
+	// 将字符串中的英文字母转为小写字母 
+	void str_tolower(char s[])
+	{
+		int i = 0;
+		while (s[i]) {
+			s[i] = tolower(s[i]);
+			i++;
+		}
+	}
+
+	思路:while(*s++){if(is_lower(s)) *s -= 26; }
+		while(*s++){if(is_uper(s)) *s += 26; }
+*/
+
+#include <stdio.h>
+
+void str_toupper(char *s);
+void str_tolower(char *s);
+
+int main(void)
+{
+	char str[128] = {0};
+	printf("请输入英文字符串:");
+	scanf(" %s", str);
+
+	str_tolower(str);
+	printf("全小写: \"%s\" \n", str);
+	str_toupper(str);
+	printf("全大写: \"%s\" \n", str);
+
+	return 0;
+}
+
+void str_toupper(char *s)
+{
+	// while(*s++)/* 吞头 */
+	for(; *s != '\0'; s++)
+	{
+		/* 是小写字母 */
+		if(*s >= 'a' && *s <= 'z')
+		{
+			/* ascii英文字母转大写 */
+			*s -= 32; 
+		}
+	}
+}
+
+void str_tolower(char *s)
+{
+	// while(*s++)/* 吞头 */
+	for(; *s != '\0'; s++)
+	{
+		/* 是大写字母 */
+		if(*s >= 'A' && *s <= 'Z')
+		{
+			/* ascii英文字母转小写 */
+			*s += 32;
+		}
+	}
+}
--- week8/exercise11_8.c
+++ week8/exercise11_8.c
@@ -16,24 +16,60 @@
     思路2:这样至少要遍历两遍,最坏的情况得额外创建两个和s1等长的数组.
         直接创建两个指针(p1和p2),同时从首元素开始,p1和p2同步递增以遍历数组,
         p2每碰到一个元素都要判定是否是数字字符,p1不动,p2自增一次(指向下一个元素),
-        再判定p2所指是否是...直到p2所指不是数字字符,自此开始将p2所指元素赋给p1,
+        再判定p2所指是否是...直到p2所指不是数字字符,将p2所指元素赋给p1,
         直到p2碰到'\0':将'\0'复制给p1所指,然后p1自增以p1将剩余元素全部写'\0'.
 
 */
 
+#include <stdio.h>
+#include <string.h>
+
 int is_digit(const char *s);/* 返回1表示是数字字符,返回0表示不是 */
 void del_digit(char *str);
 
+int main(void)
+{
+    char string[128] = {0};
+    printf("请输入字符串:\n");
+    scanf(" %127s", string);
 
+    del_digit(string);
+    
+    printf("去除数字字符后: \"%s\" \n", string);
+
+    return 0;
+}
 
 
 void del_digit(char *str)
 {
     int len = strlen(str) + 1;
-    int i = 0;
-    char *tmp = str;
+    int n = 0;
+    const char * const start = str;/* 指向原数组的头 */
+    const char * const end = str + len;/* 指向原数组的末位后一位,不可解引用和修改 */
+    char *p_effective = str;/* 用来写有效内容的指针 */
+    char *p_explorer = str;/* 用来"探路"的指针 */
 
-    
+    for( ; p_explorer < end ; p_effective++, p_explorer++ )
+    {
+        /* 先判定p_explorer指向的是否是数字字符,是则继续探索:步进一次,再判断 */
+        /* 并且避免p_explorer越界访问 */
+        /* 先检查边界,确定未越界再访问:p_explorer < end必须在前面 */
+        while( p_explorer < end && is_digit(p_explorer) == 1 )
+        {
+            p_explorer++;
+        }
+
+        /* 直到所指非数字,将p_explorer所指元素(非数字)赋值给p_effective所指元素 */
+        *p_effective = *p_explorer;
+    }
+    /* 最终p_explorer一定指向原字符串末尾的'\0' */
+
+    /* 覆写原字符串,防止信息残留 */
+    for( ; p_effective < end; p_effective++)
+    {
+        *p_effective = '\0';
+    }
 
 }
 
